ea3fc72e504a0cc3f2bb6d2259be27a9803757e7
maikol8-10 <maikol8-10@hotmail.com>
2022-02-08 12:02:44 -0600

    Some Changes Before Deployment

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ mongoose.connect(process.env.DB_URL, {
 
 const app = express();
 
+//Carpeta Publica
+app.use(express.static('uploads'))
+
 //Habilitar bodyParser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -42,9 +45,6 @@ app.use(cors(corsOptions));
 
 //Rutas de la App
 app.use('/', routes());
-
-//Carpeta Publica
-app.use(express.static('uploads'))
 
 const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 5000;
