69ae70db4eacfaf6c8595512a1b1be690384a511
maikol8-10 <maikol8-10@hotmail.com>
2022-02-08 11:09:32 -0600

    Build Deployment

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+require('dotenv').config({ path: 'variables.env' });
 //Cors permite que un cliente se conecte a un servidor para el intercambio de recursos
 const cors = require('cors');
 
 //Conectar mongo
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/restapis', {
+mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true
 })
 
@@ -20,8 +20,25 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//Definir un dominio(s) para recibir las peticiones
+const whitelist = [process.env.FRONTEND_URL];
+//Opciones de CORS
+const corsOptions = {
+
+    origin: (origin, callback) => {
+        //console.log(origin);
+        //Revisar si la petición viene de un servidor que está en la whitelist
+        const existe = whitelist.some(dominio => dominio === origin);
+        if (existe) {
+            callback(null, true);
+        } else {
+            callback(new Error('NO permitido por CORS'));
+        }
+    }
+}
+
 //Hablilitar CORS
-app.use(cors());
+app.use(cors(corsOptions));
 
 //Rutas de la App
 app.use('/', routes());
@@ -29,5 +46,11 @@ app.use('/', routes());
 //Carpeta Publica
 app.use(express.static('uploads'))
 
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 5000;
+
 //Puerto  
-app.listen(5000);
+//app.listen(5000);
+app.listen(port, host, () => {
+    console.log('El sevidor esta funcionando');
+});
--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,6 @@ module.exports = (req, res, next) => {
 
     //Autorización por medio del header
     const authHeader = req.get('Authorization');
-    console.log(authHeader);
 
     if (!authHeader) {
         const error = new Error('No autenticado, no hay JWD');
